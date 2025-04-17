7b3cd387f547eac0854e4f98d9650bae345765dd
Marc Angelo G. Primo <primomarcangelo@gmail.com>
2025-04-17 17:20:54 +0800

    From kernel to routes/console

--- routes/console.php
+++ routes/console.php
@@ -8,31 +8,29 @@ use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+Schedule::call(function () {
+    $response = Http::get("http://api.weatherstack.com/current", [
+        'access_key' => '4fe4d4c552a4b275c730f5247fbc12dd',
+        'query' => 'Licab, Nueva Ecija',
+    ]);
+
+    if ($response->successful()) {
+        $data = $response->json();
+        $precip = $data['current']['precip'] ?? 0;
+
+        if ($precip > 0) {
+            Log::info("🌧️ It's currently raining in Licab. Precipitation: {$precip} mm");
+        } else {
+            Log::info("☀️ No rain in Licab at the moment.");
+        }
+    } else {
+        Log::error("⚠️ Failed to fetch weather data");
+    }
+})->everyMinute();
+
+
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule::call(function () {
-//     $response = Http::get("http://api.weatherstack.com/current", [
-//         'access_key' => 'ZGXCYVYQ44P8YTYQ28V3C56VG',
-//         'query' => 'Licab, Nueva Ecija',
-//     ]);
-
-//     if ($response->successful()) {
-//         $data = $response->json();
-//         $precip = $data['current']['precip'] ?? 0;
-
-//         // ✅ Conditional logic
-//         if ($precip > 0) {
-//             Log::info("It's currently raining in Licab. Precipitation: {$precip} mm");
-//         } else {
-//             Log::info("No rain in Licab at the moment.");
-//         }
-
-//         // ✅ You can also save to DB, notify, etc.
-//         // WeatherData::create([...]);
-//     } else {
-//         Log::error("Failed to fetch weather data");
-//     }
-// })->everyMinute();
