d9de75de57757cff93a808b3f714c6b69a820bad
Marc Angelo G. Primo <primomarcangelo@gmail.com>
2025-04-18 10:08:23 +0800

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -107,9 +107,14 @@ return [
     | used globally for all emails that are sent by your application.
     |
     */
+    // 'from' => [
+    //     'address' => env('MAIL_FROM_ADDRESS', 'primo.marc@clsu2.edu.ph'),
+    //     'name' => env('MAIL_FROM_NAME', 'Rain Alert System'),
+    // ],
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'primo.marc@clsu2.edu.ph'),
-        'name' => env('MAIL_FROM_NAME', 'Rain Alert System'),
+        'address' => 'primo.marc@clsu2.edu.ph',
+        'name' => 'Rain Alert System',
     ],
+
 
 ];
