e73499e715d01d134a1cddfa511ab5d0840e0015
Marc Angelo G. Primo <primomarcangelo@gmail.com>
2025-04-20 22:51:22 +0800

    new

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -12,7 +12,7 @@ class FirebaseService
     public function __construct()
     {
         $factory = (new Factory)
-            ->withServiceAccount(config_path('firebase_credentials.json'))
+            ->withServiceAccount(config_path('licupdate-6a3fb-firebase-adminsdk-fbsvc-1ff312c4e0.json'))
             ->withDatabaseUri('https://licupdate-6a3fb-default-rtdb.asia-southeast1.firebasedatabase.app/');
 
         $this->database = $factory->createDatabase();
